4ab63fbe0aeffe3cd7be3aeba5cd3a56a0e53b74
Charles Tack <gfaxxlos@students.codeinstitute.net>
2025-01-14 15:14:14 +0000

    add updated_on to Post model

--- codestar/settings.py
+++ codestar/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-charlestack-testjan14th-1h8zjt553yo.ws.codeinstitute-ide.net', '.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', '8000-charlestack-testjan14th-1h8zjt553yo.ws.codeinstitute-ide.net', '.herokuapp.com']
 
 
 # Application definition
