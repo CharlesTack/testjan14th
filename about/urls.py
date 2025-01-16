5b15a48e961cb8631056abf0141964ffcec2599a
Charles Tack <gfaxxlos@students.codeinstitute.net>
2025-01-16 21:06:49 +0000

    delete about and start again

--- about/urls.py
+++
@@ -1,7 +0,0 @@
-from . import views
-from django.urls import path
-
-urlpatterns = [
-    path('', views.AdminList.as_view(), name='home'),
-    path('<str:title>/', views.post_detail, name='about_title'),
-]
--- about/views.py
+++
@@ -1,4 +0,0 @@
-from django.shortcuts import render, get_object_or_404
-from django.views import generic
-
-# Create your views here.
