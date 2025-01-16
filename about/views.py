ba3250e47c743525c5fd7c589164cad3c9144195
Charles Tack <gfaxxlos@students.codeinstitute.net>
2025-01-16 21:18:46 +0000

    update admin and views (About)

--- about/views.py
+++ about/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from .models import About
 
 # Create your views here.
+
+class PostList(generic.ListView):
+    
+    template_name = "about/about.html"
+    paginate_by = 6
+
+
+
+def about_me(request):
+    """
+    Renders the About page
+    """
+    about = About.objects.all().order_by('-updated_on').first()
+    
+    return render(
+        request,
+        "about/about.html",
+        {"about": about},
+    )
