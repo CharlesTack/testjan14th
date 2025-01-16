91b63fd3a27667b97bda7a150b40f13efdd09ab4
Charles Tack <gfaxxlos@students.codeinstitute.net>
2025-01-16 21:03:41 +0000

    amend About admin and models code to hopefully fix makemigrate issue

--- about/admin.py
+++ about/admin.py
@@ -6,11 +6,7 @@ from django_summernote.admin import SummernoteModelAdmin
 @admin.register(About)
 class AboutAdmin(SummernoteModelAdmin):
 
-    list_display = ('title', 'updated_on')
-    search_fields = ['title', 'content']
-    list_filter = ('updated_on')
     summernote_fields = ('content',)
 
 
 # Register your models here.
-admin.site.register(About)
